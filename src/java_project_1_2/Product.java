5df2b21deb412d2e078055c46c0146a320098bd8
wesle <79055@rocteraa-student.nl>
2018-09-24 13:16:25 +0200

    This is a media project

---
+++ src/java_project_1_2/Product.java
@@ -0,0 +1,41 @@
+
+package java_project_1_2;
+
+
+public class Product {
+    private int id;
+    private String name;
+    private float price;
+    private String addDate;
+    private byte[] picture;
+    
+    public Product(int pid, String pname, float pprice, byte[] pimg){
+       this.id = pid;
+       this.name = pname;
+       this.price = pprice;
+       
+       this.picture = pimg;
+    }
+
+   
+
+ 
+    
+    public int getId(){
+        return id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public float getPrice(){
+        return price;
+    }
+    
+    
+    public byte[] getImage(){
+        return picture;
+    }
+    
+}
