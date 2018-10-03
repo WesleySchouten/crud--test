ce936b62ee5ec7b47ffe64660aeb5659e5658bc0
Wesley <wesle@192.168.20.20>
2018-10-03 08:51:36 +0200

    Multiple Imputs for more fields

--- src/java_project_1_2/Product.java
+++ src/java_project_1_2/Product.java
@@ -4,35 +4,45 @@ package java_project_1_2;
 
 public class Product {
     private int id;
-    private String name;
-    private float price;
-    private String addDate;
+    private String firstName;
+    private String lastName;
+    private String woonPlaats;
+    private String Addres;
+    private int telefoonNummer;
+   
    
     
-    public Product(int pid, String pname, float pprice){
+    public Product(int pid, String pfirstName, String plastName, String pwoonPlaats, String pAddres, int ptelefoonNummer){
        this.id = pid;
-       this.name = pname;
-       this.price = pprice;
+       this.firstName = pfirstName;
+       this.lastName = plastName;
+       this.woonPlaats = pwoonPlaats;
+       this.Addres = pAddres;
+       this.telefoonNummer = ptelefoonNummer;
        
        
     }
-
-   
-
- 
-    
     public int getId(){
         return id;
     }
     
-    public String getName(){
-        return name;
+    public String getfirstName(){
+        return firstName;
+    }
+     public String getlastName(){
+        return lastName;
+    }
+      public String getwoonPlaats(){
+        return woonPlaats;
     }
     
-    public float getPrice(){
-        return price;
-    }
     
+    public String getaddres(){
+        return Addres;
+    }
+    public int gettelefoonNummer(){
+        return telefoonNummer;
+    }
     
    
     
